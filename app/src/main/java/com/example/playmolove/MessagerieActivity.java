3d7b56907f51e79d8a297012938065497e403ccc
leagu <arthur.fen-chong@esme.fr>
2021-03-22 16:47:14 +0100

    22/03

--- app/src/main/java/com/example/playmolove/MessagerieActivity.java
+++ app/src/main/java/com/example/playmolove/MessagerieActivity.java
@@ -2,7 +2,9 @@ package com.example.playmolove;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.text.SpannableStringBuilder;
 
 public class MessagerieActivity extends AppCompatActivity {
 
@@ -10,5 +12,6 @@ public class MessagerieActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_messagerie);
+
     }
 }
--- app/src/main/java/com/example/playmolove/loginActivity.java
+++ app/src/main/java/com/example/playmolove/loginActivity.java
@@ -10,42 +10,4 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 public class loginActivity extends AppCompatActivity {
-
-    private EditText username;
-    private EditText password;
-    private Button login;
-    private EditText p_lost;
-    private Button signup;
-
-    @SuppressLint("WrongViewCast")
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-
-        username = findViewById(R.id.identifiant);
-        password = findViewById(R.id.mdp);
-        login = findViewById(R.id.connexion);
-        p_lost = findViewById(R.id.mdp_oublie);
-        signup = findViewById(R.id.inscrire);
-
-        login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String inputName = username.getText().toString();
-                String inputPassword = password.getText().toString();
-
-                if (inputName.isEmpty() || inputPassword.isEmpty())
-                {
-                    Toast.makeText(this,"Entrer les données correspondantes correctement", Toast.LENGTH_SHORT).show();
-                }
-                else
-                {
-
-                }
-                }
-        })
-
-
-    }
 }
