40b6508a802672fcd3957f55beacffe02da3ed9c
Julien <julien.bordas@esme.fr>
2021-03-22 18:34:33 +0100

    Commit3

--- app/src/main/java/com/example/playmolove/GeolocActivity.java
+++ app/src/main/java/com/example/playmolove/GeolocActivity.java
@@ -1,48 +1,39 @@
 package com.example.playmolove;
 
-
-import androidx.annotation.RequiresApi;
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.app.Activity;
 import android.content.Context;
+import android.location.Criteria;
 import android.location.LocationManager;
 import android.location.LocationProvider;
 import android.os.Build;
 import android.os.Bundle;
 
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
 import java.util.ArrayList;
 
 @RequiresApi(api = Build.VERSION_CODES.M)
-public class GeolocActivity extends AppCompatActivity {
+public abstract class GeolocActivity<names, name> extends AppCompatActivity {
     LocationManager locationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
     ArrayList<LocationProvider> providers = new ArrayList<LocationProvider>();
-    ArrayList<String> names = locationManager.getProviders(true);
-    for(String name: names)
-            providers.add(locationManager.getProvider(name));
+    ArrayList<String> names = (ArrayList<String>) locationManager.getProviders(true);
+
+    for (String name:names)
+        providers.add(locationManager.getProvider(name))
 
     Criteria critere = new Criteria();
+// Critères de précision
+    critere.setAccuracy(Criteria.ACCURACY_FINE); // critère de haut précision
+    critere.setAltitudeRequired(true) // donne une altitude
+    critere.setBearingRequired(true) // donner une direction
+    critere.setSpeedRequired(true) // donne une vitesse
+    critere.setCostAllowed(false) // le fournisseur ne peut être payant
 
-// Pour indiquer la précision voulue
-// On peut mettre ACCURACY_FINE pour une haute précision ou ACCURACY_COARSE pour une moins bonne précision
-    critere.setAccuracy(Criteria.ACCURACY_FINE);
 
-// Est-ce que le fournisseur doit être capable de donner une altitude ?
-    critere.setAltitudeRequired(true);
+// Critères consommation d'énergie demandée
+    critere.setPowerRequirement(Criteria.POWER_HIGH) // haute consommation d'énergie
 
-// Est-ce que le fournisseur doit être capable de donner une direction ?
-    critere.setBearingRequired(true);
-
-// Est-ce que le fournisseur peut être payant ?
-    critere.setCostAllowed(false);
-
-// Pour indiquer la consommation d'énergie demandée
-// Criteria.POWER_HIGH pour une haute consommation, Criteria.POWER_MEDIUM pour une consommation moyenne et Criteria.POWER_LOW pour une basse consommation
-    critere.setPowerRequirement(Criteria.POWER_HIGH);
-
-// Est-ce que le fournisseur doit être capable de donner une vitesse ?
-    critere.setSpeedRequired(true);
-
+    abstract String getBestProvider(Criteria criteria, boolean enabledOnly);
 
     @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
